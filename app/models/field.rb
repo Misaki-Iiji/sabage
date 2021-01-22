0d998c55a54cbe45a115adf8aa3e158995d451b3
Misaki-Iiji <misahiro33@gmail.com>
2021-01-22 17:09:08 +0000

    いいね途中、ヘッダーフッターレイアウト（仮）

--- app/models/field.rb
+++ app/models/field.rb
@@ -7,14 +7,14 @@ class Field < ApplicationRecord
   validates :name, presence: true, length: { maximum: 20 }
   validates :introduction, length: { maximum: 300 }
 
-  has_many :pictures
+  has_many :posts
   has_many :field_favos
-
-  def favorited_by?(current_user)
-    field_favos.where(user_id: user.id).exists?
-  end
-
+  
   attachment :image #refile使うときのルール
   accepts_attachments_for :pictures, attachment: :picture
+  
+  def favorited_by?(user)
+    field_favos.where(user_id: user.id).exists?
+  end
 
 end
