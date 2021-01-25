c525de15a961f0f6197c6f78acc28e689382fd9c
Misaki-Iiji <misahiro33@gmail.com>
2021-01-25 11:33:34 +0000

    fieldテーブルカラム追加

--- db/migrate/20210120065207_add_image_id_to_fields.rb
+++ db/migrate/20210120065207_add_image_id_to_fields.rb
@@ -1,5 +1,9 @@
 class AddImageIdToFields < ActiveRecord::Migration[5.2]
   def change
     add_column :fields, :image_id, :string
+    add_column :fields, :introduction, :text
+    add_column :fields, :address, :text
+    add_column :fields, :user_id, :integer
+    remove_column :fields, :image, :string
   end
 end
