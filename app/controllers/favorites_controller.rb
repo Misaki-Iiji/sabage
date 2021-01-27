669b5d8ce6f96ee9a5c716f39bee6900c6bf30d4
Misaki-Iiji <misahiro33@gmail.com>
2021-01-27 06:46:28 +0000

    いいね非同期通信途中

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,17 +5,17 @@ class FavoritesController < ApplicationController
   end
 
   def create
-    field = Field.find(params[:field_id])
-    favorite = current_user.favorites.new(field_id: field.id)
+    @field = Field.find(params[:field_id])
+    favorite = current_user.favorites.new(field_id: @field.id)
     favorite.save
-    redirect_back(fallback_location: root_path)#うまくいかなかった場合ルートパスに飛ぶ
+    # redirect_back(fallback_location: root_path)#うまくいかなかった場合ルートパスに飛ぶ、非同期通信では必要ない
   end
 
   def destroy
-    field = Field.find(params[:field_id])
-    favorite = current_user.favorites.find_by(field_id: field.id)
+    @field = Field.find(params[:field_id])
+    favorite = current_user.favorites.find_by(field_id: @field.id)
     favorite.destroy
-    redirect_back(fallback_location: root_path)#うまくいかなかった場合ルートパスに飛ぶ
+    # redirect_back(fallback_location: root_path)#うまくいかなかった場合ルートパスに飛ぶ、非同期通信では必要ない
   end
 
 
