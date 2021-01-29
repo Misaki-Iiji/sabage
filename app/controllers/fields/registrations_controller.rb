6fa651491c9b34eaf1257cdaaba1266a661823f0
Misaki-Iiji <misahiro33@gmail.com>
2021-01-29 12:53:17 +0000

    ログイン画面レイアウト

--- app/controllers/fields/registrations_controller.rb
+++ app/controllers/fields/registrations_controller.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 class Fields::RegistrationsController < Devise::RegistrationsController
-
    before_action :configure_permitted_parameters_fields, if: :devise_controller?
+
+  def destroy #論理削除
+    resource.soft_delete
+    Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
+    set_flash_message :notice, :destroyed
+    yield resource if block_given?
+    respond_with_navigational(resource){ redirect_to after_sign_out_path_for(resource_name) }
+  end
 
   protected
 
