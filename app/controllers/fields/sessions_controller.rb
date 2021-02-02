4bfb0b47debb5f27e2f861192d70c52293603f8c
Misaki-Iiji <misahiro33@gmail.com>
2021-02-02 03:39:17 +0000

    レイアウト調整

--- app/controllers/fields/sessions_controller.rb
+++ app/controllers/fields/sessions_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class Fields::SessionsController < Devise::SessionsController
-  
+
   def after_sign_in_path_for(resource)
-    mypage_field_path(current_field) #ログイン後ページ
+    field_path(current_field) #ログイン後ページ
   end
 
 end
