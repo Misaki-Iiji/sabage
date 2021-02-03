641e27119e8ce2d2d68146bfbffb7551db8369f8
Misaki-Iiji <misahiro33@gmail.com>
2021-02-03 14:43:00 +0000

    自己紹介カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_02_072526) do
+ActiveRecord::Schema.define(version: 2021_02_03_134453) do
 
   create_table "chats", force: :cascade do |t|
     t.integer "user_id"
@@ -104,6 +104,10 @@ ActiveRecord::Schema.define(version: 2021_02_02_072526) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
+    t.string "image_id"
+    t.string "main_gun"
+    t.string "sub_gun"
+    t.string "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
