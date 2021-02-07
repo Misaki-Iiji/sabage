441a79371ce90cc8addfa0d24e5b81f8fff0e21b
Misaki-Iiji <misahiro33@gmail.com>
2021-02-07 10:37:24 +0000

    グループ機能追加

--- app/controllers/joins_controller.rb
+++ app/controllers/joins_controller.rb
@@ -1,17 +1,19 @@
 class JoinsController < ApplicationController
-  
+
   def create
-    chat_group = ChatGroup.find(params[:chat_group_id])
-    join = current_user.joins.new(chat_group_id: chat_group.id)
+    @chat_group = ChatGroup.find(params[:chat_group_id])
+    @group_chat = GroupChat.new(chat_group_id: @chat_group.id)
+    @group_chats = @chat_group.group_chats.page(params[:page])
+    join = current_user.joins.new(chat_group_id: @chat_group.id)
     join.save
-    redirect_to chat_group_path(chat_group)
   end
 
   def destroy
-    chat_group = ChatGroup.find(params[:chat_group_id])
-    join = current_user.joins.find_by(chat_group_id: chat_group.id)
+    @chat_group = ChatGroup.find(params[:chat_group_id])
+    @group_chat = GroupChat.new(chat_group_id: @chat_group.id)
+    @group_chats = @chat_group.group_chats
+    join = current_user.joins.find_by(chat_group_id: @chat_group.id)
     join.destroy
-    redirect_to chat_group_path(chat_group)  
   end
-    
+
 end
