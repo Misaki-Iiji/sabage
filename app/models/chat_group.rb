27925fab711076df7286f4da4f6a2d899f5bd512
Misaki-Iiji <misahiro33@gmail.com>
2021-02-10 07:45:59 +0000

    モデルテスト完了

--- app/models/chat_group.rb
+++ app/models/chat_group.rb
@@ -3,10 +3,13 @@ class ChatGroup < ApplicationRecord
   has_many :chat_group_to_users
   has_many :users, through: :chat_group_to_users
   has_many :joins, dependent: :destroy
-  
+
+  validates :chat_group_name, presence: true, length: { maximum: 20 }
+  validates :chat_group_description, presence: true, length: { maximum: 200 }
+
+
   def joined_by?(user)
     joins.where(user_id: user.id).exists?
   end
 
-  # accepts_nested_attributes_for :users
 end
---
+++ spec/models/infomation_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Infomation, "モデルに関するテスト", type: :model do
+  before do
+    @field = FactoryBot.create(:field)
+  
+    @infomation = Infomation.create(
+      title: "title",
+      body: "text",
+      )
+  end
+
+  it "タイトル、本文、外部キーが有効な状態であること" do
+    infomation = Infomation.new(
+    title: "title",
+    body: "text",
+    field_id: 1
+    )
+    expect(infomation).to be_valid
+  end
+
+  it "タイトルがない場合、無効である" do
+    infomation = Infomation.new(title: nil)
+    infomation.valid?
+  end
+
+  it "本文がない場合、無効である"  do
+    infomation = Infomation.new(body: nil)
+    infomation.valid?
+  end
+
+  it "本文がない場合、無効である" do
+    infomation = Infomation.new(body: nil)
+    infomation.valid?
+  end
+
+end
