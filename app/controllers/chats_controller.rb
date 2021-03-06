fce46353c97991a71dd61162a7cc2eea9b4a4663
Misaki-Iiji <misahiro33@gmail.com>
2021-03-06 09:45:14 +0000

    チャット一覧追加、ハンバーガーメニュー固定

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,7 +2,7 @@ class ChatsController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @rooms = current_user.user_rooms.pluck(:room_id)
+    @rooms = current_user.rooms
   end
 
   def show
