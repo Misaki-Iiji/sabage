d11d600bf87ea13b904d5437bd2fb13d241c1c02
Misaki-Iiji <misahiro33@gmail.com>
2021-03-14 09:40:17 +0000

    チャット通知追加

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -4,18 +4,20 @@ class Chat < ApplicationRecord
   has_many :notifications, dependent: :destroy
 
 
-  def create_notification_chat!(current_user, chat_id)
+  def create_notification_chat!(current_user, chat_id, room_id)
     # チャットしている相手を取得し、通知を送る
-    temp_ids = Chat.select(:user_id).where(room_id: id).where.not(user_id: current_user.id).distinct
+    temp_ids = Chat.select(:user_id).where(room_id: room_id).where.not(user_id: current_user.id).distinct
     temp_ids.each do |temp_id|
       save_notification_chat!(current_user, chat_id, temp_id['user_id'])
     end
+     # もしチャットが空だったら、投稿者に通知を送る
+    save_notification_chat!(current_user, chat_id, user_id) if temp_ids.blank?
   end
 
   def save_notification_chat!(current_user, chat_id, visited_id)
-    # チャットは複数回することが考えられるため、１つの投稿に複数回通知する
+    # チャットは複数回することが考えられるため、複数回通知する
     notification = current_user.active_notifications.new(
-      room_id: id,
+      # room_id: id,
       chat_id: chat_id,
       visited_id: visited_id,
       action: 'chat'
