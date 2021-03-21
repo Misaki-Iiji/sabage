8a4195b0d334406eb9b91727c63dc4ab22b3b598
Misaki-Iiji <misahiro33@gmail.com>
2021-03-21 08:35:16 +0000

    チャット機能見直し

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,10 +1,6 @@
 class ChatsController < ApplicationController
   before_action :authenticate_user!
 
-  def index
-    @rooms = current_user.rooms
-  end
-
   def show
     @user = User.find(params[:id])
     # ログインしているユーザーのidが入ったroom_idのみを配列で取得（該当するroom_idが複数でも全て取得）
